9f43227d0e3d995f199df01532c8fa981e6ee434
zbretz <zachary.bretz@gmail.com>
2024-08-15 22:39:34 -0600

    Added 'need help / phone support' button to ride detail

--- tecumseh/user.js
+++ tecumseh/user.js
@@ -8,7 +8,7 @@ const { db__, db_locals } = require('./mongoConnection.js')
 
 
 let onDemandActive = true
-let live_versions = ['6.0.0', '6.0.1']
+let live_versions = ['6.0.1', '6.1.0']
 
 router.get('/populateData', async (req, res) => {
 
