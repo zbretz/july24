afb0d51bbefff4a143c6b3713de1da36c6dd67e3
zbretz <zachary.bretz@gmail.com>
2024-08-28 09:46:01 -0600

    Disabling font-scaling for text and text input components

--- geronimo/index.js
+++ geronimo/index.js
@@ -1,10 +1,13 @@
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
 // import { Text, AppRegistry } from 'react-native';
 
-// import { Text } from 'react-native';
+import { Text, TextInput } from 'react-native';
 
-// Text.defaultProps = Text.defaultProps || {};
-// Text.defaultProps.allowFontScaling = false;
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.allowFontScaling = false;
+
+TextInput.defaultProps = TextInput.defaultProps || {};
+TextInput.defaultProps.allowFontScaling = false;
 
 import App from './CoreNav/App';
 
