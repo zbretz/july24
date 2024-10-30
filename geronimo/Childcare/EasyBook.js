4e518bfb79f932150b1473fa99bb055c7dd3d6f7
zbretz <zachary.bretz@gmail.com>
2024-10-30 13:55:05 -0600

    Added registration/login process to frontend booking flow

--- geronimo/Childcare/EasyBook.js
+++ geronimo/Childcare/EasyBook.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 const windowHeight = Dimensions.get('window').height;
 const windowWidth = Dimensions.get('window').width;
 
-export default EasyBook = ({ masterState, setMasterState, sitter, navigation, booking, setBooking }) => {
+export default EasyBook = ({ masterState, setMasterState, sitter=null, navigation, booking, setBooking }) => {
 
 
     const [age1, setAge1] = useState(null)
@@ -29,11 +29,16 @@ export default EasyBook = ({ masterState, setMasterState, sitter, navigation, bo
 
     const request = { age1, age2, age3, age4, dateTime, notes }
 
-    const bookNow = () => {
+    const bookNow = async () => {
+
+        if (!masterState.user) {
+            await signIn()
+        }
+
+        // Alert.alert('Booking Placed', JSON.stringify(request));
 
 
-        signIn()
-        return
+        // console.log('proceed with booking')
 
 
         setBooking(booking => {
@@ -43,6 +48,10 @@ export default EasyBook = ({ masterState, setMasterState, sitter, navigation, bo
         })
 
         booking && navigation.navigate('Booking')
+
+
+
+
 
 
         // axios.post(`${locals_url}/locals/placeOrder`, { user: masterState.user, basket, timeOfOrder: timeOfOrder, useWallet })
@@ -65,15 +74,20 @@ export default EasyBook = ({ masterState, setMasterState, sitter, navigation, bo
         // .catch(e => console.log('order  error: ', e))    }
 
 
-        Alert.alert('Order Placed', JSON.stringify(request));
 
     }
 
 
 
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [phone, setPhone] = useState('')
+    // const [firstName, setFirstName] = useState('')
+    // const [lastName, setLastName] = useState('')
+    // const [phone, setPhone] = useState('')
+
+    const [firstName, setFirstName] = useState('cvcv')
+    const [lastName, setLastName] = useState('cvcvc')
+    const [phone, setPhone] = useState('9175751955')
+
+
     const [error, setError] = useState()
 
     const [modalVisible, setModalVisible] = useState(false)
@@ -95,24 +109,72 @@ export default EasyBook = ({ masterState, setMasterState, sitter, navigation, bo
         return first.length && last.length && (first.length + last.length > 4)
     };
 
-    const signIn = () => {
+    const signIn = async () => {
 
         if (!isValidName(firstName, lastName)) { errorTimeout("Please enter a valid name"); return }
         if (!isValidPhone(phone)) { errorTimeout("Please enter valid phone number"); return }
 
         console.log('first name: ', firstName)
 
-        axios.post(`${url}/auth/childcareSignIn`, { user: { firstName, lastName, phone } })
-            .then(res => {
-                if (res.data) {
-                    console.log('register user response: ', res.data)
-                    setModalVisible(true)
-                }
-                // else {
-                //     console.log('registration error!')
-                //     errorTimeout("Phone number taken.\nTry signing in!")
-                // }
-            })
+        let signIn = await axios.post(`${url}/auth/childcareSignIn`, { user: { firstName, lastName, phone } })
+
+        console.log('sign in response: ', signIn.data)
+        // .then(res => {
+        //     if (res.data) {
+        //         console.log('register user response: ', res.data)
+
+        await awaitCode()
+        console.log('stop waiting')
+
+
+        //     }
+        // })
+
+
+
+    }
+
+    const promiseref = useRef(null)
+
+    const awaitCode = () => {
+        setModalVisible(true)
+        return new Promise((resolve, reject) => {
+
+            promiseref.current = resolve
+            // setTimeout(() => {
+            //     setModalVisible(false)
+            //     resolve()
+            // }, 3000)
+
+
+        })
+    }
+
+
+
+    const [code, setCode] = useState('')
+
+    const enterCode = (code) => {
+
+        setCode(code)
+
+        if (code.length === 4) {
+            //     promiseref.current()
+            // }
+            axios.post(`${url}/auth/childcareCode`, { phone: phone, code })
+                .then(async (res) => {
+                    if (res.data.status == 'ok') {
+                        console.log('user code response: ', res.data.user)
+                        setMasterState(masterState => ({ ...masterState, user: res.data.user }))
+                        promiseref.current('logged in')
+                        setModalVisible(false)
+                    } else {
+                        setCode('')
+                        errorTimeout('Incorrect Code')
+                    }
+
+                })
+        }
 
     }
 
@@ -123,7 +185,44 @@ export default EasyBook = ({ masterState, setMasterState, sitter, navigation, bo
 
         <View style={{ width: '100%', }}>
 
-            <CodeModal phone={phone} modalVisible={modalVisible} setModalVisible={setModalVisible} setMasterState={setMasterState} />
+            <Modal
+                animationType='slide'
+                transparent={true}
+                visible={modalVisible}
+                style={{ height: windowHeight, width: windowWidth, }}>
+
+                <View style={{ height: windowHeight, width: windowWidth, backgroundColor: 'rgba(0,0,0,.6)', }}>
+
+                    <TouchableOpacity onPress={() => { console.log('close modal'); setModalVisible(false) }} style={{ position: 'absolute', height: '100%', width: '100%', backgroundColor: 'transparent', }} />
+
+                    <View style={{ backgroundColor: '#f2f2f2', top: windowHeight * .1, alignSelf: 'center', borderRadius: 20, padding: 20, justifyContent: 'center' }}>
+                        <View style={{ backgroundColor: '#e6e6e6', borderRadius: 20, padding: 20, alignItems: 'center', justifyContent: 'center', }}>
+
+                            {/* <TouchableOpacity onPress={async () => { await enterCode(); console.log('post-promise') }} style={{ height: 100, width: 100, backgroundColor: 'green', }} /> */}
+                            <TouchableOpacity onPress={() => { promiseref.current(); console.log('post-promise') }} style={{ height: 100, width: 100, backgroundColor: 'green', }} />
+
+                            <View style={{ borderRadius: 30, }}>
+                                <>
+                                    <Image style={{ height: 90, width: 90, alignSelf: 'center' }} source={require('../assets/unlock.png')} />
+                                    <Text style={{ fontSize: 20, color: '#353431', textAlign: 'center', fontFamily: 'Aristotelica-Regular', margin: 10 }}>Please enter the verification code sent to your phone.</Text>
+                                    <TextInput
+                                        keyboardType='numeric'
+                                        autoCapitalize={'none'}
+                                        autoFocus={true}
+                                        placeholderTextColor={'#000'}
+                                        value={code}
+                                        onChangeText={code => enterCode(code)}
+                                        style={{ marginTop: 16, height: 40, backgroundColor: '#fff', borderRadius: 20, textAlign: 'center', fontSize: 20, width: 100, alignSelf: 'center' }}
+                                    />
+                                </>
+                                {error && <Text style={{ color: '#000', fontSize: 22, fontFamily: 'Aristotelica-Regular', textAlign: 'center', marginTop: 20 }}>Incorrect Code</Text>}
+                            </View>
+                        </View>
+                    </View>
+
+                </View>
+            </Modal>
+            {/* <CodeModal code={code} setCode={setCode} setBigDog={setBigDog} bigDog={bigDog} phone={phone} modalVisible={modalVisible} setModalVisible={setModalVisible} setMasterState={setMasterState} /> */}
 
             <View style={{ marginVertical: 30, marginHorizontal: 20 }}>
                 <View style={{
@@ -314,8 +413,8 @@ export default EasyBook = ({ masterState, setMasterState, sitter, navigation, bo
                     }
 
                     {error &&
-                        <View style={{ backgroundColor: '#f2f2f2', padding: 10, borderRadius: 10, alignSelf:'center', marginTop: 10, }}>
-                            <Text style={{fontWeight:'500', color: 'red', fontSize: 20 , textAlign:'center'}}>{error}</Text>
+                        <View style={{ backgroundColor: '#f2f2f2', padding: 10, borderRadius: 10, alignSelf: 'center', marginTop: 10, }}>
+                            <Text style={{ fontWeight: '500', color: 'red', fontSize: 20, textAlign: 'center' }}>{error}</Text>
                         </View>
                     }
 
@@ -334,10 +433,9 @@ export default EasyBook = ({ masterState, setMasterState, sitter, navigation, bo
 
 
 
-const CodeModal = ({ modalVisible, setModalVisible, phone, setMasterState }) => {
+const CodeModal = ({ modalVisible, setModalVisible, phone, setMasterState, setBigDog, bigDog, code, setCode }) => {
 
     const userRef = useRef()
-    const [code, setCode] = useState('')
     const [error, setError] = useState()
 
 
@@ -362,7 +460,7 @@ const CodeModal = ({ modalVisible, setModalVisible, phone, setMasterState }) => 
                         setModalVisible(false)
                         console.log('closing mmodal')
                         setMasterState(masterState => ({ ...masterState, user: res.data.user }))
-                       
+
                     } else {
                         setCode('')
                         errorTimeout('Incorrect Code')
@@ -371,6 +469,47 @@ const CodeModal = ({ modalVisible, setModalVisible, phone, setMasterState }) => 
                 })
         }
     }
+    console.log('big dog: ', bigDog)
+
+    // useEffect(() => {
+
+
+    //     const awaitCode = () => {
+    //         setModalVisible(true)
+
+    //         return new Promise((resolve, reject) => {
+    //             if (bigDog) {
+    //                 console.log('yahoo')
+    //                 resolve()
+    //             }
+    //         })
+
+    //     }
+
+    // }, [bigDog])
+
+
+    // useEffect(() => {
+
+
+    //     const awaitCode = () => {
+
+    //         return new Promise((resolve, reject) => {
+    //             setModalVisible(true)
+
+    //             if (bigDog) {
+    //                 console.log('yahoo')
+    //                 resolve()
+    //             }
+    //         })
+
+    //     }
+
+    //     // awaitCode()
+
+    //     // return ()=>setBigDog(false)
+
+    // }, [bigDog])
 
 
     return (
@@ -387,6 +526,9 @@ const CodeModal = ({ modalVisible, setModalVisible, phone, setMasterState }) => 
 
                 <View style={{ backgroundColor: '#f2f2f2', top: windowHeight * .1, alignSelf: 'center', borderRadius: 20, padding: 20, justifyContent: 'center' }}>
                     <View style={{ backgroundColor: '#e6e6e6', borderRadius: 20, padding: 20, alignItems: 'center', justifyContent: 'center', }}>
+
+                        <TouchableOpacity onPress={() => { setBigDog(true) }} style={{ height: 100, width: 100, backgroundColor: 'green', }} />
+
                         <View style={{ borderRadius: 30, }}>
                             <>
                                 <Image style={{ height: 90, width: 90, alignSelf: 'center' }} source={require('../assets/unlock.png')} />
