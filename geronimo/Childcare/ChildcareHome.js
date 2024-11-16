24ae4ae0241c621c648e7c5a9ca199e4572afa91
zbretz <zachary.bretz@gmail.com>
2024-11-16 14:05:43 -0700

    Added a meet-the-team carousel component (not fully developed)

--- geronimo/Childcare/ChildcareHome.js
+++ geronimo/Childcare/ChildcareHome.js
@@ -33,7 +33,7 @@ export default ChildcareHome = ({ masterState, setMasterState, navigation, provi
                     </View>
                 </TouchableOpacity> */}
 
-<TouchableOpacity onPress={() => scrollRef.current.scrollTo({x:windowWidth*.18})} style={{ padding: 20, marginTop: 20, flexDirection: 'row', position: 'absolute', zIndex: 1 }} name="arrow-back-ios" size={24} color="black" >
+                <TouchableOpacity onPress={() => scrollRef.current.scrollTo({ x: windowWidth * .19 + 30 })} style={{ padding: 20, marginTop: 20, flexDirection: 'row', position: 'absolute', zIndex: 1 }} name="arrow-back-ios" size={24} color="black" >
                     <MaterialIcons style={{ marginLeft: 10, paddingTop: 10, marginLeft: -10 }} name="arrow-back-ios" size={24} color="#fff" />
                     <View>
                         <Text style={{ fontFamily: 'PointSoftSemiBold', fontSize: 32, marginTop: 0, color: '#fff' }}>Childcare</Text>
@@ -46,13 +46,15 @@ export default ChildcareHome = ({ masterState, setMasterState, navigation, provi
                     <Image style={{ height: 40, width: 40, borderRadius: 30 }} source={require('../assets/yellow-icon-bold.png')} />
                 </View> */}
 
-                <ScrollView ref={scrollRef} pagingEnabled={true} contentContainerStyle={{alignItems:'center'}} showsHorizontalScrollIndicator={false} horizontal style={{width: windowWidth * .23, position: 'absolute',  marginTop: 10, alignSelf: 'flex-end', backgroundColor: 'rgba(0,0,0,.3)', borderTopLeftRadius: 40, borderBottomLeftRadius: 40, marginTop: 30, padding: 12, }}>
+                <Text style={{ position: 'absolute', top: 50, right: 0, fontFamily: 'PointSoftSemiBold', fontSize: 13, marginRight: 6, color: '#fff', textAlign: 'center' }}>Meet The Team</Text>
+
+                <ScrollView ref={scrollRef} pagingEnabled={true} contentContainerStyle={{ alignItems: 'center' }} showsHorizontalScrollIndicator={false} horizontal style={{ width: windowWidth * .19 + 30, position: 'absolute', alignSelf: 'flex-end', backgroundColor: 'rgba(0,0,0,.3)', borderTopLeftRadius: 60, borderBottomLeftRadius: 60, marginTop: 70, padding: 0, }}>
                     <View style={{ flexDirection: 'row', }}>
                         {Object.values(providers).slice(0, 3).map((sitter, idx) => {
                             return (
 
-                                <TouchableOpacity onPress={() => navigation.navigate('SitterList')} key={idx} style={{ alignItems: 'center', justifyContent: 'center', marginHorizontal: 4 }} name="arrow-back-ios" size={24} color="black" >
-                                    <Image style={{ width: windowWidth * .17, height: windowWidth * .17, borderRadius: 40 }} source={{ uri: sitter.cover_photo }} />
+                                <TouchableOpacity onPress={() => navigation.navigate('SitterList')} key={idx} style={{ alignItems: 'center', justifyContent: 'center', marginRight: 30 }} name="arrow-back-ios" size={24} color="black" >
+                                    <Image style={{ width: windowWidth * .19, height: windowWidth * .19, borderRadius: 40, }} source={{ uri: sitter.cover_photo }} />
                                 </TouchableOpacity>
                             )
                         })}
@@ -97,6 +99,7 @@ export default ChildcareHome = ({ masterState, setMasterState, navigation, provi
                 </LinearGradient>
 
 
+
                 <View style={{ backgroundColor: '#fff' }}>
                     <EasyBook masterState={masterState} navigation={navigation} setMasterState={setMasterState} />
                 </View>
@@ -122,6 +125,53 @@ export default ChildcareHome = ({ masterState, setMasterState, navigation, provi
                 <View style={{ backgroundColor: '#fff' }}>
                     <Faq />
                 </View>
+
+
+                {/* <View style={{ justifyContent: 'center', backgroundColor: '#fff' }} >
+                    <View style={{ borderRadius: 10, marginTop: 30, padding: 0}}>
+
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', marginTop: 0 }}>
+                            {Object.values(providers).slice(0, 1).map((sitter, idx) => {
+                                return (
+
+                                    <TouchableOpacity onPress={() => navigation.navigate('SitterList')} key={idx} style={{ alignItems: 'center', justifyContent: 'center'}} name="arrow-back-ios" size={24} color="black" >
+                                        <Image style={{ width: windowWidth * .9, height: windowWidth * .7, borderRadius: 3 0 }} source={{ uri: sitter.cover_photo }} />
+                                        <Text style={{ fontSize: 30, textAlign: 'center', color: '#000', position:'absolute', color:'white' }}>Meet the Team</Text>
+                                    </TouchableOpacity>
+                                )
+                            })}
+                        </View>
+
+                    </View>
+                </View>
+
+                <View style={{ backgroundColor:'transparent', top:-70 }}>
+                    <EasyBook masterState={masterState} navigation={navigation} setMasterState={setMasterState} />
+                </View> */}
+
+
+             
+
+                {/* 
+<View>
+
+<Video
+                ref={video}
+                style={{ height: windowHeight, width: windowWidth, zIndex: -1 }}
+                // source={require('../assets/childcare_cover.mp4')}
+                source={require('../assets/cover_tall.mp4')}
+                // source={{ uri: 'https://theparkcityapp.s3.us-east-1.amazonaws.com/istockphoto-1369478616-640_adpp_is.mp4' }}
+                // useNativeControls
+                resizeMode={ResizeMode.COVER}
+                isLooping
+                // shouldPlay
+                isMuted
+            />
+
+
+
+
+</View> */}
 
 
 
